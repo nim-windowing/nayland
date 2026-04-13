a3ef65b411a442c23bd016e64a80a76d4b3d8a4f
xTrayambak <xtrayambak@gmail.com>
2026-04-13 16:40:26 +0530

    add: core/pointer: implement all events

--- src/nayland/types/protocols/core/pointer.nim
+++ src/nayland/types/protocols/core/pointer.nim
@@ -21,6 +21,10 @@ type
     frameCb: PointerFrameCallback
     axisCb: PointerAxisCallback
     axisSourceCb: PointerAxisSourceCallback
+    axisStopCb: PointerAxisStopCallback
+    axisDiscreteCb: PointerAxisDiscreteCallback
+    axisValue120Cb: PointerAxisValue120Callback
+    axisRelativeDirection: PointerAxisRelativeDirectionCallback
     buttonCb: PointerButtonCallback
 
   PointerCallbackPRef = ref PointerCallbackPayload
@@ -36,9 +40,9 @@ type
   PointerMotionCallback* = proc(pntr: Pointer, time: uint32, surfaceX, surfaceY: float)
   PointerAxisSourceCallback* = proc(pntr: Pointer, axisSource: uint32)
   PointerAxisStopCallback* = proc(pntr: Pointer, time, axis: uint32)
-  PointerAxisDiscreteCallback* = proc(pntr: Pointer, axis, discrete: int32)
-  PointerAxisValue120Callback* = proc(pntr: Pointer, axis, value120: int32)
-  PointerAxisRelativeDirection* = proc(pntr: Pointer, axis, direction: uint32)
+  PointerAxisDiscreteCallback* = proc(pntr: Pointer, axis: uint32, discrete: int32)
+  PointerAxisValue120Callback* = proc(pntr: Pointer, axis: uint32, value120: int32)
+  PointerAxisRelativeDirectionCallback* = proc(pntr: Pointer, axis, direction: uint32)
   PointerButtonCallback* =
     proc(pntr: Pointer, serial, time, button: uint32, state: ButtonState)
 
@@ -83,6 +87,24 @@ let listener = wl_pointer_listener(
   ) {.cdecl.} =
     let payload = cast[ptr PointerCallbackPayload](data)
     payload.buttonCb(payload.obj, serial, time, button, cast[ButtonState](state)),
+  axis_stop: proc(data: pointer, pntr: ptr wl_pointer, time, axis: uint32) {.cdecl.} =
+    let payload = cast[ptr PointerCallbackPayload](data)
+    payload.axisStopCb(payload.obj, time, axis),
+  axis_discrete: proc(
+      data: pointer, pntr: ptr wl_pointer, axis: uint32, discrete: int32
+  ) {.cdecl.} =
+    let payload = cast[ptr PointerCallbackPayload](data)
+    payload.axisDiscreteCb(payload.obj, axis, discrete),
+  axis_value120: proc(
+      data: pointer, pntr: ptr wl_pointer, axis: uint32, value120: int32
+  ) {.cdecl.} =
+    let payload = cast[ptr PointerCallbackPayload](data)
+    payload.axisValue120Cb(payload.obj, axis, value120),
+  axis_relative_direction: proc(
+      data: pointer, pntr: ptr wl_pointer, axis, direction: uint32
+  ) {.cdecl.} =
+    let payload = cast[ptr PointerCallbackPayload](data)
+    payload.axisRelativeDirection(payload.obj, axis, direction),
 )
 
 proc release*(pntr: Pointer | PointerObj) =
@@ -108,6 +130,20 @@ proc `onAxisSource=`*(pntr: Pointer, callback: PointerAxisSourceCallback) =
 
 proc `onButton=`*(pntr: Pointer, callback: PointerButtonCallback) =
   pntr.callbacks.buttonCb = callback
+
+proc `onAxisStop=`*(pntr: Pointer, callback: PointerAxisStopCallback) =
+  pntr.callbacks.axisStopCb = callback
+
+proc `onAxisDiscrete=`*(pntr: Pointer, callback: PointerAxisDiscreteCallback) =
+  pntr.callbacks.axisDiscreteCb = callback
+
+proc `onAxisValue120=`*(pntr: Pointer, callback: PointerAxisValue120Callback) =
+  pntr.callbacks.axisValue120Cb = callback
+
+proc `onAxisRelativeDirection=`*(
+    pntr: Pointer, callback: PointerAxisRelativeDirectionCallback
+) =
+  pntr.callbacks.axisRelativeDirection = callback
 
 proc attachCallbacks*(pntr: Pointer) =
   pntr.callbacks.obj = pntr
