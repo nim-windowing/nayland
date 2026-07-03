4a7547d5e2932f6035a7ba57ca26e96aa9b9316d
xTrayambak <xtrayambak@gmail.com>
2026-07-03 21:00:59 +0530

    fix: wlr/layer_shell: import `prelude` for `xdg_shell`, not `wm_base`

--- src/nayland/types/protocols/wlr/layer_shell/prelude.nim
+++ src/nayland/types/protocols/wlr/layer_shell/prelude.nim
@@ -7,7 +7,7 @@
 ## Clients can use this interface to assign the surface_layer role to wl_surfaces. Such surfaces are assigned to a "layer" of the output and rendered with a defined z-depth respective to each other. They may also be anchored to the edges and corners of a screen and specify input handling semantics. This interface should be suitable for the implementation of many desktop shell components, and a broad number of other applications that interact with the desktop.
 import
   pkg/nayland/types/protocols/wlr/layer_shell/[constants, shell, surface],
-  pkg/nayland/types/protocols/xdg_shell/wm_base
+  pkg/nayland/types/protocols/xdg_shell/prelude
     # HACK: Required because wlr-layer-shell pulls in the interface for xdg-popup, which in turn requires compiling the private code for xdg-shell.
 
 export constants, shell, surface
